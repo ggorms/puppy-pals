b187838557bf2883ece29e19ae7bc844785ec059
Garrett Gorman <garrettgorman1@gmail.com>
2023-08-28 12:46:04 -0400

    basic functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-
 import './App.css'
+import { useState } from 'react'
+import {puppyList} from './data.js'
 
 function App() {
- 
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log(puppies)
 
   return (
-    <>
-      
-    </>
+      <div>
+        {
+          puppies.map((puppy) => {
+            return <p key = {puppy.id}> {puppy.name} </p>
+          })
+        }
+
+      </div>
   )
 }
 
