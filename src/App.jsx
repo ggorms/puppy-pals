e730908686bb7f4657b5d4e5520ceedab220fce0
Garrett Gorman <garrettgorman1@gmail.com>
2023-08-28 14:29:41 -0400

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,40 @@
 import './App.css'
 import { useState } from 'react'
 import {puppyList} from './data.js'
+import './index.css'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
-  console.log(puppies)
+  const [featPupId, setFeatPupId] = useState(null)
+
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId) 
+  console.log(featuredPup)
+  // console.log(puppies)
 
   return (
-      <div>
+      <div className='container'>
+        <div className='main'>
         {
           puppies.map((puppy) => {
-            return <p key = {puppy.id}> {puppy.name} </p>
+            return <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>
+              {puppy.name}
+              </p>
           })
+          
+          
         }
-
+        </div>
+       
+        {featPupId && (
+        <div className='sub'>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+        
       </div>
   )
 }
